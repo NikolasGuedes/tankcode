6e27376f1d0a2260a299a9976b53a4ea6fb031c4
Nikolas Guedes <guedesnikolas@gmail.com>
2025-08-24 19:19:13 -0300

    feat: add new controller and some adjusts in routes

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -12,9 +12,9 @@ import { Head, Link } from '@inertiajs/vue3';
         class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
         <header class="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl">
             <nav class="flex items-center justify-end gap-4">
-                <Link v-if="$page.props.auth.user" :href="route('dashboard')"
+                <Link v-if="$page.props.auth.user" :href="route('students')"
                     class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]">
-                Dashboard
+                students
                 </Link>
                 <template v-else>
                     <Link :href="route('login')"
