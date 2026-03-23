22a0057e91b44eac788415d5717f2c35e22ae84e
Nikolas Guedes <guedesnikolas@gmail.com>
2026-03-22 22:42:15 -0300

    feat: ensure account deletion test verifies soft deletion of user

--- tests/Feature/Settings/ProfileUpdateTest.php
+++ tests/Feature/Settings/ProfileUpdateTest.php
@@ -64,7 +64,9 @@ test('user can delete their account', function () {
         ->assertRedirect('/');
 
     $this->assertGuest();
-    expect($user->fresh())->toBeNull();
+    $this->assertSoftDeleted('users', [
+        'id' => $user->id,
+    ]);
 });
 
 test('correct password must be provided to delete account', function () {
